e75967717e8121a13ebebad945b50ed7898f1566
mulloa90 <marioulloa@Marios-iMac.attlocal.net>
2022-07-24 09:53:21 -0700

    updated Html

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@ var currentDate = document.querySelector(".date");
 var currentTime = document.querySelector(".time");
 var searchForm = document.querySelector("#search-form");
 var API_KEY = "533d85454b838c8602df5b7476173c44";
-
-var cityName = "Atlanta";
+var currentWeatherItemsEl = document.getElementById("current-weather-item");
+var cityName = "San Diego";
 
 currentDate.textContent = new Date().toLocaleString("en-US", {
   weekday: "long",
@@ -42,7 +42,9 @@ function getLatandLon(e) {
 searchForm.addEventListener("submit", getLatandLon);
 
 function fetchWeather(lat, lon) {
-  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+  )
     .then((result) => result.json())
     .then((data) => {
       console.log(data);
@@ -50,4 +52,39 @@ function fetchWeather(lat, lon) {
     .catch((err) => {
       console.log(err);
     });
+}
+
+function weatherData() {
+  navigator.geolocation.getCurrentPosition((success) => {
+    let { latitude, longitude } = success.coords;
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`
+    )
+      .then(result.json())
+      .then((data) => {
+        console.log(data);
+        displayWeatherData(data);
+      });
+  });
+}
+
+function displayWeatherData() {
+  let { humidity, pressure, sunrise, sunset, wind_speed } = data.current;
+  currentWeatherItemsEl.innerHTML = ` <div class="weather-item">
+<div>Humidity</div>
+<div>${humidity}%</<div>
+ <div class="weather-item">
+<div>Pressure</div>
+<div>${pressure}</<div>
+<div class="weather-item">
+<div>Sunrise</div>
+<div>${window.moment(sunrise * 1000).format('HH:mm a')}</<div>
+<div class="weather-item">
+<div>Sunset</div>
+<div>${window.moment(sunset * 1000).format('HH:mm a')}</<div>
+<div class="weather-item">
+<div>Wind Speed</div>
+<div>${wind_speed}</<div>
+`;
 }
