ec4f5bd76b77731cee8c0573772969c623574c1d
mulloa90 <marioulloa@Marios-iMac.attlocal.net>
2022-07-24 13:06:02 -0700

    updated forecast

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
-var currentDate = document.querySelector(".date");
-var currentTime = document.querySelector(".time");
+var currentDate = document.querySelector(".date-container");
 var searchForm = document.querySelector("#search-form");
 var API_KEY = "533d85454b838c8602df5b7476173c44";
 var currentWeatherItemsEl = document.getElementById("current-weather-item");
@@ -12,12 +11,6 @@ currentDate.textContent = new Date().toLocaleString("en-US", {
   year: "numeric",
 });
 
-currentTime.textContent = `${new Date().toLocaleTimeString("en-US", {
-  hour12: true,
-  hour: "numeric",
-  minute: "numeric",
-})}`;
-
 function getLatandLon(e) {
   e.preventDefault();
   fetch(
@@ -28,11 +21,8 @@ function getLatandLon(e) {
     })
     .then((data) => {
       console.log(data);
-      //console log the lat
-      console.log(data[0].lat);
-      //console log the lon
-      console.log(data[0].lon);
-      fetchWeather(data[0].lat, data[0].lon);
+      fetchCurrentWeather(data[0].lat, data[0].lon);
+      fetchForecast(data[0].lat, data[0].lon);
     })
     .catch((err) => {
       console.log(err);
@@ -41,9 +31,9 @@ function getLatandLon(e) {
 
 searchForm.addEventListener("submit", getLatandLon);
 
-function fetchWeather(lat, lon) {
+function fetchCurrentWeather(lat, lon) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`
   )
     .then((result) => result.json())
     .then((data) => {
@@ -54,20 +44,36 @@ function fetchWeather(lat, lon) {
     });
 }
 
-function weatherData() {
-  navigator.geolocation.getCurrentPosition((success) => {
-    let { latitude, longitude } = success.coords;
-
-    fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`
-    )
-      .then(result.json())
-      .then((data) => {
-        console.log(data);
-        displayWeatherData(data);
-      });
-  });
+function fetchForecast(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${API_KEY}`
+  )
+    .then((result) => result.json())
+    .then((data) => {
+      // console.log(data.list);
+      for (let i = 5; i < data.list.length; i += 8) {
+        console.log(data.list[i]);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
+
+// function weatherData() {
+//   navigator.geolocation.getCurrentPosition((success) => {
+//     let { latitude, longitude } = success.coords;
+
+//     fetch(
+//       `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${API_KEY}`
+//     )
+//       .then(result.json())
+//       .then((data) => {
+//         console.log(data);
+//         displayWeatherData(data);
+//       });
+//   });
+// }
 
 function displayWeatherData() {
   let { humidity, pressure, sunrise, sunset, wind_speed } = data.current;
@@ -79,10 +85,10 @@ function displayWeatherData() {
 <div>${pressure}</<div>
 <div class="weather-item">
 <div>Sunrise</div>
-<div>${window.moment(sunrise * 1000).format('HH:mm a')}</<div>
+<div>${window.moment(sunrise * 1000).format("HH:mm a")}</<div>
 <div class="weather-item">
 <div>Sunset</div>
-<div>${window.moment(sunset * 1000).format('HH:mm a')}</<div>
+<div>${window.moment(sunset * 1000).format("HH:mm a")}</<div>
 <div class="weather-item">
 <div>Wind Speed</div>
 <div>${wind_speed}</<div>
